af22e223a51b8fd1eaf798404fd10f6e9d060495
Akmat <sirazhidinovakmatali@gmail.com>
2018-12-26 23:53:00 +0600

    I add event keyboard left,up,down,right

--- index.js
+++ index.js
@@ -4,14 +4,41 @@ var app = new Vue({
 		rightOperandArray: [],
 		leftOperandArray: [],
 		rightOperand: 2,
-		leftOperand: 2
+		leftOperand: 2,
+		result: '?'
 	},
 
-	computed: {
-		result: function () {
-			this.leftOperand = this.leftOperandArray[Math.floor(Math.random() * this.leftOperandArray.length)];
-			this.rightOperand = this.rightOperandArray[Math.floor(Math.random() * this.rightOperandArray.length)];
-			return this.leftOperand * this.rightOperand;
+	methods: {
+		left: function () {
+			this.result = this.rightOperand * this.leftOperand;
+		},
+		up: function () {
+			this.result = this.rightOperand * this.leftOperand;
+		},
+		down: function () {
+			this.result = this.rightOperand * this.leftOperand;
+		},
+		right: function () {
+			this.result = this.rightOperand * this.leftOperand;
 		}
 	}
 });
+
+window.addEventListener('keydown', function(event) {
+  switch (event.keyCode) {
+    case 37:
+      app.left();
+    break;
+    case 38:
+      app.up();
+    break;
+    case 39:
+      app.down();
+    break;
+    case 40:
+      app.right();
+    break;
+  }
+}, false);
+
+
